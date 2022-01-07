dea83cbfd7e12b996bd067c2664e66db1489cb5e
root <root@DESKTOP-KDA7N8U.localdomain>
2022-01-07 09:52:26 +0100

    fixed wrapper bug

--- neuman.cpp
+++ neuman.cpp
@@ -11,9 +11,8 @@ class Wrapped{
     public:
         double cyl_neumann(double a, double b){
             double res;
-            res = std::cyl_neumann(a, b);
-            std::cout << res << std::endl;
-            return res;
+            return std::cyl_neumann(a, b);
+      
         }
 
         double cyl_bessel_j(double a, double b){
@@ -25,7 +24,7 @@ class Wrapped{
 
 
 extern "C" {
-    __declspec(dllexport)
+    // __declspec(dllexport)
     Wrapped* wrapped_new() { return new Wrapped();}
     double Wrapped_cyl_neumann( Wrapped* wrapped, double a, double b){ return wrapped->cyl_neumann(a,b);}
     double Wrapped_cyl_bessel_j( Wrapped* wrapped, double a, double b){ return wrapped->cyl_bessel_j(a,b);}
--- wrapper.py
+++ wrapper.py
@@ -1,14 +1,19 @@
 from ctypes import cdll
 from ctypes import c_double
 lib = cdll.LoadLibrary('/home/lucdierick/FWI-python/neumann.so')
- 
+
 class Wrapper(object):
     def __init__(self):
+        lib.Wrapped_cyl_neumann.restype = c_double
+        lib.Wrapped_cyl_bessel_j.restype = c_double
         self.obj = lib.wrapped_new()
  
-    def cyl_neumann(self, a ,b)->c_double:
-        return c_double(lib.Wrapped_cyl_neumann(self.obj , c_double(a),c_double(b)))
+    def cyl_neumann(self, a ,b):
+        return lib.Wrapped_cyl_neumann(self.obj , c_double(a),c_double(b))
     
-    def cyl_bessel_j(self, a, b)->c_double:
-        return c_double(lib.Wrapped_cyl_bessel_j(self.obj, c_double(a), c_double(b)))
+    def cyl_bessel_j(self, a, b):
+        return lib.Wrapped_cyl_bessel_j(self.obj, c_double(a), c_double(b))
       
+
+f = Wrapper()
+print(f.cyl_neumann(2.342,0.2343))
