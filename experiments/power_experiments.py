1f50f1b2432fc4b652d400d1e739024ab7edbbce
Luc Dierick <ladierick@gmail.com>
2022-03-30 14:01:17 +0200

    latest changes

--- experiments/power_experiments.py
+++ experiments/power_experiments.py
@@ -16,6 +16,8 @@ estimated_100 = [2.002, 2.002, 2.002]
 estimated_20 = [1.857, 1.857, 1.857]
 
 total_power = []
+total_energy = [] #This is the execution time * power
+execution_time = []
 total_object = []
 total_ambient = []
 idle_power = []
@@ -23,7 +25,14 @@ idle_ambient = []
 idle_object = []
 memory = []
 
+execution_timeGPU = [0.18665,3.87273/10,19.0606/50]
+execution_timePY = [0.45947837829589844,3.07403302192688/10, 11.48944640159607/50]
+gpu_power = []
+gpu_idle_power = []
+
 total_power_20 = []
+total_energy_20 = [] #This is the execution time * power
+execution_time_20 = []
 total_object_20 = []
 total_ambient_20 = []
 idle_power_20 = []
@@ -31,10 +40,54 @@ idle_ambient_20 = []
 idle_object_20 = []
 memory_20 = []
 
+
+gpu = []
+with open(f"{win}/experiments/output_1000_303025.log", "r") as f:
+    next(f)
+    for line in f:
+        gpu.append(float(line[:-2]))
+np_gpu = np.array(gpu)
+max = np.max(np_gpu)
+min = np.min(np_gpu)
+print(f"3030 max: {max} min: {min}")
+
+gpu = []
+with open(f"{win}/experiments/output_1000_505025.log", "r") as f:
+    next(f)
+    for line in f:
+        gpu.append(float(line[:-2]))
+np_gpu = np.array(gpu)
+max = np.max(np_gpu)
+min = np.min(np_gpu)
+print(f"5050 max: {max} min: {min}")
+
+with open(f"{win}/experiments/output_20000.log", "r") as f:
+    next(f)
+    for line in f:
+        gpu.append(float(line[:-2]))
+np_gpu = np.array(gpu)
+max = np.max(np_gpu)
+min = np.min(np_gpu)
+print(f"2k max: {max} min: {min}")
+
 for i in l:
+
+    gpu = []
+    with open(f"{win}/experiments/output_{i*10}.log","r") as f:
+        next(f)
+        for line in f:
+            gpu.append(float(line[:-2]))
+    np_gpu =  np.array(gpu)
+    max = np.max(np_gpu)
+    min = np.min(np_gpu)
+    gpu_power.append(np.mean(np_gpu[np_gpu>=(max-1.5)]))
+    gpu_idle_power.append(np.mean(np_gpu[np_gpu<=(min+1.5)]))
     data = np.loadtxt(f"{win}/experiments/300_100_{i*10}.txt")
     data_t = data.T #get transpose
     memory.append(np.mean(data_t[4]))
+    execution_time.append(np.mean(data_t[0])/(i/10))
+
+
 
     data_host = np.loadtxt(f"{win}/experiments/300_100_{i*10}_host.txt")
     data_host_t = data_host.T
@@ -44,6 +97,7 @@ for i in l:
     idle_power.append(np.mean(data_host_t[3]))
     idle_object.append(np.mean(data_host_t[4]))
     idle_ambient.append(np.mean(data_host_t[5]))
+    total_energy.append(execution_time[-1] * total_power[-1])
 
 
     ################### 20 #######################
@@ -51,6 +105,7 @@ for i in l:
     data_20 = np.loadtxt(f"{win}/experiments/300_20_{i*10}.txt")
     data_t_20 = data_20.T #get transpose
     memory_20.append(np.mean(data_t_20[4]))
+    execution_time_20.append(np.mean(data_t_20[0])/(i/10))
 
     data_host_20 = np.loadtxt(f"{win}/experiments/300_20_{i*10}_host.txt")
     data_host_t_20 = data_host_20.T
@@ -60,12 +115,43 @@ for i in l:
     idle_power_20.append(np.mean(data_host_t_20[3]))
     idle_object_20.append(np.mean(data_host_t_20[4]))
     idle_ambient_20.append(np.mean(data_host_t_20[5]))
+    total_energy_20.append(execution_time_20[-1] * total_power_20[-1])
 
-print(f"average memory: {memory}")
-print(f"average memory: {memory_20}")
 
-labels = ["High utilization", "Low utilization", "Estimated power High", "Estimated power low"]
-plt.title("Total power over gridpoints")
+# print(f"average memory: {memory}")
+# print(f"average memory: {memory_20}")
+# print(gpu_power)
+print(execution_time_20)
+print(execution_time)
+
+labels = ["Z1 High utilization", "Z1 Low utilization", "Z1 Est. High Utilization", "Z1 Est. Low Utilization","Quadro T1000 "]
+plt.title("Total Energy over gridpoints")
+plt.xlabel("Gridpoints")
+plt.ylabel("Energy in (Joules/100Gridpoints)")
+
+plt.plot([x*10 for x in l],total_energy,"-bo")
+plt.plot([x*10 for x in l],total_energy_20,"-ro")
+plt.plot([x*10 for x in l],np.multiply(estimated_100,execution_time),"-y")
+plt.plot([x*10 for x in l],np.multiply(estimated_20,execution_time_20),"-g")
+plt.plot([x*10 for x in l],np.multiply(gpu_power,execution_timeGPU),"-o")
+plt.legend(labels)
+plt.show()
+
+plt.clf()
+
+plt.title("Execution times over gridpoints")
+plt.xlabel("Gridpoints")
+plt.ylabel("Execution time (s)")
+
+plt.plot([x*10 for x in l],execution_time,"-bo")
+plt.plot([x*10 for x in l],execution_time_20,"-ro")
+plt.plot([x*10 for x in l],execution_timeGPU,"-yo")
+plt.legend(["Z1 High Utilization","Z1 Low Utilization","Quadro P1000"])
+plt.show()
+
+plt.clf()
+
+plt.title("Total Power over gridpoints")
 plt.xlabel("Gridpoints")
 plt.ylabel("Power in Watt")
 
@@ -73,10 +159,12 @@ plt.plot([x*10 for x in l],total_power,"-bo")
 plt.plot([x*10 for x in l],total_power_20,"-ro")
 plt.plot([x*10 for x in l],estimated_100,"-y")
 plt.plot([x*10 for x in l],estimated_20,"-g")
+plt.plot([x*10 for x in l],gpu_power,"-o")
 plt.legend(labels)
 plt.show()
 
 plt.clf()
+
 
 plt.ylabel("Temperature in Celsius")
 plt.xlabel("Gridpoints")
@@ -105,6 +193,7 @@ plt.plot([x*10 for x in l],idle_power,"-bo")
 plt.plot([x*10 for x in l],idle_power_20,"-ro")
 plt.plot([x*10 for x in l],estimated_100,"-y")
 plt.plot([x*10 for x in l],estimated_20,"-g")
+plt.plot([x*10 for x in l],gpu_idle_power,"-o")
 plt.legend(labels)
 plt.show()
 plt.clf()
